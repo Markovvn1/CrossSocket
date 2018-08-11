7e9aacc1053a0612c16ef350f176e8cc32e2fb9b
Markovvn1 <32509100+Markovvn1@users.noreply.github.com>
2018-08-11 18:15:21 +0700

    Create client.cpp

---
+++ example/client.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+#include "socket.hpp"
+
+using namespace std;
+
+int main()
+{
+    Socket socket;
+    socket.open();
+    socket.connect("127.0.0.1", 1111);
+
+    char data1[13];
+    socket.recv(data1, 13);
+    cout << data1 << endl;
+
+    const char* data2 = "Hello, Vova!";
+    socket.send(data2, strlen(data2) + 1);
+
+    return 0;
+}
